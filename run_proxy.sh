f40e33709c36e60130042d0102eee117261187e6
FforFrodo <FforFrodo@users.noreply.github.com>
2023-01-13 12:36:30 +0100

    added shell script

---
+++ run_proxy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script runs the python script "proxy.py"
+
+# Navigate to the directory where the script is located
+cd /desktop/git/n26/n33/
+
+# Make the script executable
+chmod +x simple_proxy.py
+
+# Run the script using the python interpreter
+python3 simple_proxy.py
